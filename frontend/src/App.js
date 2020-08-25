fd4efb0bfeec19c031b6286ab1c19b97ca1a087e
msmsarian <michaelsmsarian@gmail.com>
2020-08-25 17:19:26 +1000

    this is my first push ligma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
+import FAQ from './pages/FAQ';
 import AddCard from './pages/AddCard'
 import { Router } from '@reach/router'
 
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Home path="/" />
       <AddCard path="/card/add" />
+      <FAQ path="/faq" />
     </Router>
   );
 }
---
+++ frontend/src/pages/FAQ.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => (
+
+
+    <div className="FAQ">
+        <h1>FAQ </h1>
+        <ul>
+            <li>Instagram</li>
+            <li>WhatsApp</li>
+            <li>Oculus</li>
+        </ul>
+    </div>
+
+
+)
