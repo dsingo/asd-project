1c7e1aa2c6b8ee3e4cc59931870a681b7562df52
dsingo <david.singleton.65@gmail.com>
2020-08-25 15:31:53 +1000

    please work please just GO TO TEAMS NOW PLEASE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          fuck codeship all my homies hate codeship A LOT DUDE oh my god dude, now lets test teams again ghvsghvadghvawhhhy please work
+          im at peace
         </p>
         <a
           className="App-link"
