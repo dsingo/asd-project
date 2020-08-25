5c0782ab4663599e66079e5fa69dcf4e5de57efb
dsingo <david.singleton.65@gmail.com>
2020-08-25 14:35:10 +1000

    let us test teams integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          fuck codeship all my homies hate codeship A LOT DUDE oh my god dude
+          fuck codeship all my homies hate codeship A LOT DUDE oh my god dude, now lets test teams
         </p>
         <a
           className="App-link"
