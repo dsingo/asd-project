eedfef4bdbce96946d437ffc57e6e1fa65332bf8
dsingo <david.singleton.65@gmail.com>
2020-08-25 13:40:38 +1000

    curse you codeship

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          fuck codeship all my homies hate codeship
         </p>
         <a
           className="App-link"
