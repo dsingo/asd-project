1debe5f5d87bc0f577fd63dd07ad4b6acd645f1d
dsingo <david.singleton.65@gmail.com>
2020-08-25 13:44:34 +1000

    i swear on zheng yan if this doesn't work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          fuck codeship all my homies hate codeship
+          fuck codeship all my homies hate codeship A LOT DUDE
         </p>
         <a
           className="App-link"
