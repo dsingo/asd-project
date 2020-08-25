5e82d2d2f6f8a42a827609c2dd50e5657ead6162
dsingo <david.singleton.65@gmail.com>
2020-08-25 13:27:20 +1000

    testing pipeline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git Gud at React
         </a>
       </header>
     </div>
