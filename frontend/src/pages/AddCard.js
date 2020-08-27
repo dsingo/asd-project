9b97726d001f07ac1fc3e9efcdbc66e45e9fc551
dsingo <david.singleton.65@gmail.com>
2020-08-27 13:03:54 +1000

    Added card adding and card viewing, along with card context and reducers

--- frontend/src/pages/AddCard.js
+++ frontend/src/pages/AddCard.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from '@reach/router';
+import CardsContext from '../contexts/CardsContext';
 
-export default () => (
+export default () => {
 
-    
-              <>
-              test
-              </>   
-            
-    
-)
+  const { dispatch } = useContext(CardsContext);
+  const navigate = useNavigate();
+
+  const [card, setCard] = useState({
+    nickname: '',
+    type: 'Adult',
+    balance: 0,
+  });
+
+  const setNickname = (e) => {
+    const newNickname = e.target.value;
+
+    setCard({
+      ...card,
+      nickname: newNickname
+    });
+  }
+
+  const setCardType = (e) => {
+    const newCardType = e.target.value;
+
+    setCard({
+      ...card,
+      type: newCardType
+    });
+  }
+
+  const cardTypes = [
+    'Adult',
+    'Concession',
+    'Child'
+  ]
+
+  const addCard = e => {
+    e.preventDefault();
+    const action = {
+      type: 'ADD_CARD',
+      card: card
+    }
+    dispatch(action);
+    navigate('/');
+  }
+
+  return (
+    <>
+      <form>
+        <label htmlFor='nickname'>Card Nickname</label>
+        <input name='nickname' value={card.nickname} onChange={setNickname} />
+        <label htmlFor='type'>Card Type</label>
+        <select value={card.type} onChange={setCardType}>
+          { cardTypes.map((type, index) => (
+            <option key={index} value={type}>{type}</option>
+          ))}
+        </select>
+        <button type="submit" onClick={addCard}>Add New Card</button>
+      </form>
+    </>
+  )
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from '@reach/router';
+import CardsContext from '../contexts/CardsContext';
 import Button from '../components/Button'
 
 export default () => {
-  const cards = [
-    {
-      id: "1",
-      type: "Adult",
-      balance: 15.000,
-      nickname: "David"
-    },
-    {
-      id: "2",
-      type: "Child",
-      balance: 20.000,
-      nickname: "Andrew"
-    },
-  ];
+  // const cards = [
+  //   {
+  //     id: "1",
+  //     type: "Adult",
+  //     balance: 15.000,
+  //     nickname: "David"
+  //   },
+  //   {
+  //     id: "2",
+  //     type: "Child",
+  //     balance: 20.000,
+  //     nickname: "Andrew"
+  //   },
+  // ];
+
+  const { state:cards } = useContext(CardsContext);
 
   return (
     <>
       <h1>View your cards</h1>
       <ul>
-        {cards.map( card => (
+        {cards.map( (card, index) => (
           <li>
-            {card.nickname}'s Card - Balance ${card.balance}
+            <Link to={`/card/${index}`}>{card.nickname}'s Card</Link> - Balance ${card.balance} 
           </li>
         ))}
       </ul>
 
-      <Button to="/add">Add a card</Button>
+      <Button to="/card/add">Add a card</Button>
     </>
   )
 }
