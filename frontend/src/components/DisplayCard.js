0d35e485e0b3af011c47fe905aed1b76e0e1a71f
dsingo <david.singleton.65@gmail.com>
2020-09-01 13:35:16 +1000

    more shit

---
+++ frontend/src/components/DisplayCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+export default () => (
+    <svg width="157px" height="99px" viewBox="0 0 157 99" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <linearGradient x1="126.523935%" y1="17.9967446%" x2="38.6550806%" y2="58.4101013%" id="linearGradient-1">
+                <stop stop-color="#232323" offset="0%"></stop>
+                <stop stop-color="#000000" offset="100%"></stop>
+            </linearGradient>
+        </defs>
+        <g id="WayCard-Physical-Adult" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+            <rect id="Rectangle" fill="url(#linearGradient-1)" x="0" y="0" width="157" height="99" rx="8"></rect>
+            <text id="W." font-family="PlayfairDisplay-Regular_Bold, Playfair Display" font-size="44" font-weight="bold" fill="#FFFFFF">
+                <tspan x="53.0840725" y="68">W.</tspan>
+            </text>
+            <text id="Adult" font-family="Roboto-Light, Roboto" font-size="10" font-weight="300" fill="#FFFFFF">
+                <tspan x="9.12548828" y="91">Adult</tspan>
+            </text>
+        </g>
+    </svg>
+)
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,37 +2,22 @@ import React, { useContext } from 'react';
 import { Link } from '@reach/router';
 import CardsContext from '../contexts/CardsContext';
 import Button from '../components/Button'
+import CenteredViewport from '../layouts/CenteredViewport';
+import CardListItem from '../components/CardListItem';
 
 export default () => {
-  // const cards = [
-  //   {
-  //     id: "1",
-  //     type: "Adult",
-  //     balance: 15.000,
-  //     nickname: "David"
-  //   },
-  //   {
-  //     id: "2",
-  //     type: "Child",
-  //     balance: 20.000,
-  //     nickname: "Andrew"
-  //   },
-  // ];
+  // const cards = ;
 
   const { state:cards } = useContext(CardsContext);
 
   return (
-    <>
+    <CenteredViewport>
       <h1>View your cards</h1>
-      <ul>
-        {cards.map( (card, index) => (
-          <li>
-            <Link to={`/card/${index}`}>{card.nickname}'s Card</Link> - Balance ${card.balance} 
-          </li>
-        ))}
-      </ul>
+      {cards.map( (card, index) => (
+        <CardListItem card={card} key={index} />
+      ))}
 
       <Button to="/card/add">Add a card</Button>
-    </>
+    </CenteredViewport>
   )
 }
