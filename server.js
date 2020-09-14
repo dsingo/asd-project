e254098cf6519273b98559d71047a9619277a1ce
Dov <dov.royal@gmail.com>
2020-09-14 16:36:46 +1000

    Start created user model
    Start created the user schema for new users

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.get('/', (req, res) => {
 });
 
 // Define routes 
-app.use('/users', require('./routes/api/users'))
+app.use('/users', require('./routes/api/User'));
+app.use('/auth', require('./routes/api/Auth'));
+app.use('/profile', require('./routes/api/Profile'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
