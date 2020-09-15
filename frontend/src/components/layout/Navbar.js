7bb72e9291b9e6aac3969605f3e7c0c45b7c227d
Dov <dov.royal@gmail.com>
2020-09-15 11:22:36 +1000

    Tweak navbar
    Remove bottom border from navbar, make it look nicer

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./Navbar.scss"
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="dashboard.html"> <i className="fas fa-code"></i> Opal Manager </a>
+        <a href="dashboard.html">
+          {" "}Opal Manager{" "}
+        </a>
       </h1>
       <ul>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li>
+          <a href="/user">Login/Register</a>
+        </li>
       </ul>
     </nav>
   );
