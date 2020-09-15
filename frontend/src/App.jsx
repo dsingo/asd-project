ea23bd0659b0bd0c77054e4518b3125124690483
Dov <dov.royal@gmail.com>
2020-09-15 11:30:47 +1000

    Install react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 
