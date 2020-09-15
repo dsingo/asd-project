070dac9f6895818a750d3699f0f9aeb7b27cf7c2
Dov <dov.royal@gmail.com>
2020-09-15 10:32:21 +1000

    Changing home page
    Trying to make the login the home page

---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.scss"
+
+const Navbar = () => {
+  return (
+    <nav>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db')
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //Connect Database
 connectDB();
