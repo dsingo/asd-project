16768a445658d26fc1d8cc6625ccec64d4c0f3ff
Dov <dov.royal@gmail.com>
2020-09-22 13:54:58 +1000

    Set alert for password change
    Show an alert when the user enters an incorrect current password

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -152,6 +152,12 @@ export const updatePassword = ({ oldPassword, password1 }) => async dispatch => 
         dispatch(setAlert('Your password has been updated successfully', 'success'));
 
     } catch (err) {
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+        
         dispatch({ 
             type: PASSWORD_ERROR, 
             payload: { msg: err.response.statusText, status: err.response.status 
