957eb015b9f6f572f5cd478e0e84a7537effb781
Dov <dov.royal@gmail.com>
2020-09-27 23:26:59 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const connectDB = require('./config/db')
 const path = require('path')
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 //Connect Database
 connectDB();
@@ -24,5 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
