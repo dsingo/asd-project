8d0a8f67479cd231f6d7d9d42440a296d9442c67
Dov <dov.royal@gmail.com>
2020-09-27 23:14:34 +1000

    Prepare for deployment to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,12 +11,18 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
-
 // Define routes 
 app.use('/user', require('./routes/api/user'));
 app.use('/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
