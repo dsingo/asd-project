a8fea892423841493217f952761c1a4a5cc9ea52
Dov <dov.royal@gmail.com>
2020-10-15 14:53:29 +1100

    Set up faq page
    Styled the faq page so that each question is contained in a rectangle

--- frontend/src/components/layout/FAQ.js
+++ frontend/src/components/layout/FAQ.js
@@ -3,11 +3,23 @@ import "./FAQ.scss";
 
 export default () => (
     <div className="faq">
-        <h1>Frequently Asked Questions </h1>
-        <ul>
-            <li>How to use an Opal Card?</li>
-            <li>How do I top up my Opal Card?</li>
-            <li>How do I replace my Opal Card?</li>
+        <h1 className="faq-heading">Frequently Asked Questions </h1>
+        <ul className="rect">
+            <li className="qs">How to use an Opal Card?
+            <p>When you buy an opal card you can tap on and off on any public transport in NSW</p>
+            </li>
+        </ul>
+
+        <ul className="rect">
+            <li className="qs">How do I top up my Opal Card?
+            <p>When you buy an opal card you can tap on and off on any public transport in NSW</p>
+            </li>
+        </ul>
+            
+        <ul className="rect">
+            <li className="qs">How do I replace my Opal Card?
+            <p>When you buy an opal card you can tap on and off on any public transport in NSW</p>
+            </li>
         </ul>
     </div>
 )
