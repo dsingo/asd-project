436883bf74cde9f2041bcaec87680ce3bb7457e3
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-21 18:37:34 +1100

    Trial of fixing add card

--- frontend/src/actions/cards.js
+++ frontend/src/actions/cards.js
@@ -4,17 +4,17 @@ import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
 
 export const addNewCard = (card) => async (dispatch) => {
-  /*try {
+  try {
     await axios.post("/cards", { card });
 
-    dispatch({ type: ADDED_CARD, payload: card });
+    //dispatch({ type: ADDED_CARD, payload: card });
 
     console.log("success");
   } catch (err) {
-    dispatch({
-      type: FAILED_ADD_CARD,
-      payload: { msg: err.response.statusText, status: err.response.status },
-    });
+    //dispatch({
+      //type: FAILED_ADD_CARD,
+      //payload: { msg: err.response.statusText, status: err.response.status },
+    //});
     console.log("not success");
-  }*/
+  }
 };
