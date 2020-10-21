dbe6a82d5239c20ce94415219c13b7123dd74518
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-21 23:22:52 +1100

    Fixed typos and error to do with context

--- frontend/src/components/layout/ViewCards.js
+++ frontend/src/components/layout/ViewCards.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
-import CardsContext from "../contexts/CardsContext";
 
-const ViewCard = ({ id }) => {
-  const { state: cards } = useContext(CardsContext);
-  const listcards = data.map((d) => <li key={d.nickname}>{d.nickname}</li>);
+
+const ViewCard = () => {
+  const cards = [];
+  const listcards = cards.map((d) => <li key={d.nickname}>{d.nickname}</li>);
 
   return <div>{listcards}</div>;
 };
