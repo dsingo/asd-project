6e963efdd039341f5d7d5e0b662bf67a666aacac
Matthew Clifford <mattclifford2000@gmail.com>
2020-10-24 22:19:11 +1100

    Commented out possible offending code

--- frontend/src/actions/cards.js
+++ frontend/src/actions/cards.js
@@ -39,9 +39,9 @@ export const addNewCard = (card) => async (dispatch) => {
 };
 
 export const viewUserCards = (id) => async (dispatch) => {
-  const params = new URLSearchParams(["id", id]);
+  //const params = new URLSearchParams(["id", id]);
 
-  const res = await axios.get("/cards", { params });
+  //const res = await axios.get("/cards", { params });
 };
 
 export const addToCard = (id) => async (dispatch) => {};
